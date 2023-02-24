8f1dda66c0fddb2274f630a93e86cef527e0f38e
Violetta87 <athena_mewo87@hotmail.com>
2023-02-24 15:32:54 +0100

    yas

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
 //To serve static files use this function, and name root between bracket.
+
 app.use(express.static("public"));
+
+const weekDays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "satursday"]
 
 
 /**Pages */
-app.get("/frontPage", (req,res) => {
+app.get("/", (req,res) => {
     res.sendFile(__dirname + "/public/frontPage.html")
 });
 
 
 /**API */
 app.get("/api/time", (req,res) => {
-    const data = console.log("hej")
-    res.send({data: data})
+    const date = new Date();
+    const today = weekDays[date.getDay()]
+    console.log(today)
+    res.send({data: today})
 });
+
+/**port */
 
 const PORT = 8080
 
@@ -24,6 +31,5 @@ app.listen(PORT, (error) => {
         return
     }
     console.log("server is running on PORT: ", PORT)
-
 
 });
--- public/frontPage.js
+++ public/frontPage.js
@@ -1,1 +1,29 @@
-console.log("LAURA ER En PRUT")
+const url = "http://localhost:8080/api/time"
+let tis;
+    fetch(url).then(response => response.json()).then(result => {
+        tis = result.data
+    }).then(() => {
+        console.log(tis)
+    })
+
+
+function isItFriday(){
+    const divDay = document.getElementById("divDay");
+    divDay.innerText = "";
+    const createP = document.createElement("p")
+        
+        if(tis === 'friday'){
+            createP.innerText = `YES! Its ${tis}`
+        }else{
+            createP.innerText = `No its not friday ${tis} stupid`
+
+        }  
+        divDay.appendChild(createP);
+}
+
+
+
+        
+        
+
+
